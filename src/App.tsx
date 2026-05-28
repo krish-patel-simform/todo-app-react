8040315fab6f2debfa987e0e18b1cc9a4a3a216c
krish-patel-simform <krish.patel@simformsolutions.com>
2026-05-28 12:32:46 +0530

    removed unused component from import

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
 import "./App.css";
-import Modal from "./Components/Modal/Modal";
 import DashboardPage from "./pages/Dashboard/DashboardPage";
 
 function App() {
   return (
     <>
       <DashboardPage />
-      {/* <EditModal /> */}
-      {/* navbar */}
-      {/* <Modal header="Add New Task" /> */}
-
-      {/* task Container */}
-      {/* <Button title="Demo" leftIcon={<FaPlus />} />
-      <Input type="serach" leftIcon={<FaSearch />} /> */}
     </>
   );
 }
