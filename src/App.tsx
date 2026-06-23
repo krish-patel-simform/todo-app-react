dbe22e4fab57c3f50b3c093bedc2e9d73af43dac
krish-patel-simform <krish.patel@simformsolutions.com>
2026-06-23 15:15:53 +0530

    create an context for theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import DashboardPage from "./pages/Dashboard/DashboardPage";
 import AllTaskListProvider from "./Provider/AllTaskListProvider";
+import ThemeProvider from "./Provider/ThemeProvider";
 
 function App() {
   return (
     <>
       <AllTaskListProvider>
-        <DashboardPage />
+        <ThemeProvider>
+          <DashboardPage />
+        </ThemeProvider>
       </AllTaskListProvider>
     </>
   );
